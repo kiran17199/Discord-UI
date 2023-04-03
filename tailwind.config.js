61214fcf3d1efe12c4fb177c89f8b2de5072e15f
Kiran Chauhan <91481234+kiran17199@users.noreply.github.com>
2023-04-03 18:24:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+       ggSans: ["ggSans"],
+       Ginto: ["Ginto"],
+       Whitney: ["Whitney"],
+       
+       
+      },
+      colors: {
+        Blueberry: "#404eed",
+        cyanblue: "#99aab5",
+        darknotblack: "#2c2f33",
+        focusborder: "#00b0f4",
+        statusgreen: "#43b581",
+        textlink: "#00b0f4",
+        offwhite: "#f6f6f6",
+        white: "#fff",
+        notquiteblack: "#23272a",
+        notquiteblack: "#23272a",
+        black: "#000",
+        ekkored: "#de2761", 
+      },},
+  },
+  plugins: [],
+};
